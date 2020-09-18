1dac225b2dad89062be762908ef6f241d236125f
phsu40 <phsu40@gatech.edu>
2020-09-18 15:20:02 -0400

    Build the app & Products table

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-# // find tutorial for flask mysql
-# // download the mysql
-# // 
 from flask import Flask, session, render_template, redirect, request, json
-from models import db, connect_db
+from models import db, connect_db, Product
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/ga_ppe_sqlalchemy'
@@ -10,7 +7,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
-# db.create_all()
+db.create_all()
+
+# db.engine.execute('INSERT INTO Products (id, name_color, name_type) VALUES ("WHMSK","white","mask"),("BLMSK","blue","mask"),("RDMSK","red","mask"),("GRMSK","green","mask"),("WHRES","white","respirator"),("YLRES","yellow","respirator"),("ORRES","orange","repirator"),("CLSHD","clear","shield"),("GRGOG","green","goggles"),("ORGOG","orange","goggles"),("WHGOG","white","goggles"),("BKGOG","black","goggles"),("BLSHC","blue","shoe cover"),("BLHOD","blue","hood"),("BLGWN","blue","gown"),("GRSHC","green","shoe cover"),("GRHOD","green","hood"),("GRGWN","green","gown"),("GYSHC","grey","shoe cover"),("GYHOD","grey","hood"),("GYGWN","grey","gown"),("WHSHC","white","shoe cover"),("WHHOD","white","hood"),("WHGWN","white","gown"),("BKSTE","black","stethoscope"),("WHSTE","white","stethoscope"),("SISTE","silver","stethoscope"),("BKGLO","black","gloves"),("WHGLO","white","gloves"),("GRGLO","green","gloves");')
+
+
+@app.route('/')
+def show_all_products():
+    product_list = Product.query.all()
+    length = len(product_list)
+
+    return render_template('show_all_product.html', product_list=product_list, length=length)
 
 
 
--- models.py
+++ models.py
@@ -6,5 +6,24 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+class Product(db.Model):
+    """Product"""
+    __tablename__ = "Products"
+    __table_args__ = (
+        db.UniqueConstraint('name_color', 'name_type', name='unique_color_type'),
+    )
+
+    def __repr__(self):
+        product = self
+        return f"<Product id={product.id} name_color={product.name_color} name_type={product.name_type}>"
+
+    id = db.Column(db.CHAR(5), primary_key=True)
+    name_color = db.Column(db.String(30), nullable=False)
+    name_type = db.Column(db.String(30), nullable=False)
+
+
+
+
+
 
 
